350cc301498899e45cc6101ef4ff92f12fd25851
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-01-08 11:45:25 +0800

    Added pseudocode

---
+++ biggest_number_finder_program.py
@@ -0,0 +1,17 @@
+# Program that asks the user to input THREE NUMBERS ONLY.
+# Then the program itself will find and print the BIGGEST NUMBER among the three given numbers.
+
+# pseudocode
+
+# Import the Tkinter Font Art
+# Create a tkinter window with a title and size
+# Create a tkinter canvas
+# Create a tkinter font for the welcome message and the output
+# Create a tkinter label for the welcome message
+# Create tkinter entry boxes for the input numbers
+# Create a tkinter function to find the largest number
+    # Use the if-else statement to find the largest number
+    # Create a tkinter label for the output with the largest number
+    # Create a tkinter button to close the window
+# Create a tkinter button to find the largest number
+# Run the tkinter window
